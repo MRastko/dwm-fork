7cfc8fc2b5319ff37e623733b99d1623a05a84e1
MRastko <rastko.marinkovic@protonmail.com>
2023-03-06 19:47:03 -0600

    changes

--- config.h
+++ config.h
@@ -9,10 +9,10 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";	/* background color */
-static const char col_gray2[]       = "#444444";	/* inactive window color */
-static const char col_gray3[]       = "#bbbbbb";	/* font color */
-static const char col_gray4[]       = "#eeeeee";	/* background color*/
-static const char col_cyan[]        = "#005577";
+static const char col_gray2[]       = "#558C8C";	/* inactive window color */
+static const char col_gray3[]       = "#EFF7FF";	/* font color */
+static const char col_gray4[]       = "#eeeeee";	/* current tag and current window font color */
+static const char col_cyan[]        = "#E8DB7D";	/* Top bar second color and active window border color */	
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "2", "3", "4", "5", "6", "7", "8", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
